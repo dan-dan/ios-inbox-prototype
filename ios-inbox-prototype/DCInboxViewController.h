4413853e8f5ae5f862152c921020de3e028e40c0
dan-dan <danielhc2@gmail.com>
2015-12-12 19:45:47 -0800

    Changed to View Controller

---
+++ ios-inbox-prototype/DCInboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  DCInboxViewController.h
+//  ios-inbox-prototype
+//
+//  Created by Daniel Cortez on 12/12/15.
+//  Copyright © 2015 HCD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCInboxViewController : UIViewController
+
+@end
---
+++ ios-inbox-prototype/DCSettings.m
@@ -0,0 +1,11 @@
+//
+//  DCSettings.m
+//  ios-inbox-prototype
+//
+//  Created by Daniel Cortez on 12/12/15.
+//  Copyright © 2015 HCD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString *kNoteCellIdentifier = @"kNoteCellIdentifier";
